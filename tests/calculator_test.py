ec1234df5c1a65c06058566fb028e791804a2610
Jake Nhan <jtn24@njit.edu>
2022-04-19 22:07:05 -0400

    Minor change.

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,19 +3,19 @@ from calculator import Calculator
 
 
 def test_calculator_is_instance():
-    """Testing the Calculator"""
+    """Testing Calculator Instance"""
     calculator = Calculator()
     assert isinstance(calculator, Calculator)
 
 
 def test_calculator_get_result_method():
-    """Testing the Calculator"""
+    """Testing Calculator Get Result"""
     calculator = Calculator()
     assert calculator.get_result() == 0
 
 
 def test_calculator_result_property():
-    """Testing the Calculator"""
+    """Testing the Calculator's Result Property"""
     calc1 = Calculator()
     calc2 = Calculator()
     calc1.result = 5
@@ -25,11 +25,11 @@ def test_calculator_result_property():
 
 
 def test_calculator_add_method():
-    """Testing the Calculator"""
+    """Testing the Calculator's Add Method (0 + 1)"""
     calculator = Calculator()
     assert calculator.add(1) == 1
 
 def test_calculator_subtract_method():
-    """Testing the Calculator Subtract"""
+    """Testing the Calculator's Subtract Method (0 - 1)"""
     calculator = Calculator()
     assert calculator.subtract(1) == -1
