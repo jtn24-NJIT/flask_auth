f1015e62820ffa41d8af6af7f7954e10d070beea
Jake Nhan <jtn24@njit.edu>
2022-04-19 21:37:19 -0400

    Commented out requestformatter import.

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -4,7 +4,7 @@ from logging.config import dictConfig
 import flask
 from flask import request, current_app
 
-from app.logging_config.log_formatters import RequestFormatter
+# from app.logging_config.log_formatters import RequestFormatter
 
 log_con = flask.Blueprint('log_con', __name__)
 
@@ -37,7 +37,7 @@ def configure_logging():
     log = logging.getLogger("myApp")
     log.info("My App Logger")
     log = logging.getLogger("myerrors")
-    log.info("THis broke")
+    log.info("This broke")
 
 
 
