3e445c7161e0710ac90ca0e3283da8754ff33f78
Jake Nhan <jtn24@njit.edu>
2022-04-22 15:57:09 -0400

    Database fixes.

--- tests/config_test.py
+++ tests/config_test.py
@@ -2,7 +2,10 @@ def test_development_config(application):
     application.config.from_object('app.config.DevelopmentConfig')
     assert application.config['DEBUG']
     assert not application.config['TESTING']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
+    # Use for github
+    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/runner/work/flask_auth/flask_auth/database/db.sqlite'
+    # Use for local/pycharm test
+    # assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
 
 
 def test_testing_config(application):
@@ -16,4 +19,7 @@ def test_production_config(application):
     application.config.from_object('app.config.ProductionConfig')
     assert not application.config['DEBUG']
     assert not application.config['TESTING']
-    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
+    # Use for github
+    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/runner/work/flask_auth/flask_auth/database/db.sqlite'
+    # Use for local/pycharm test
+    # assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:////home/myuser/database/db.sqlite'
