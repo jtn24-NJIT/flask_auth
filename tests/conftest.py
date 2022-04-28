2f8ef86bc8782d3c570d940915acd2b4e1a71aca
Jake Nhan <jtn24@njit.edu>
2022-04-28 15:49:27 -0400

    Dashboard, song & test fixes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def add_user(application):
     """ Adding a user to the application's database """
     with application.app_context():
         #new record
-        user = User('keith@webizly.com', 'testtest')
+        user = User('keith@webizly.com', 'testtest', 0)
         db.session.add(user)
         db.session.commit()
 
